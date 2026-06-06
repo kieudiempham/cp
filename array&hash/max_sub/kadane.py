94c48460acb6d219d61c417befe7b99ac1f7ac95
Pham Thi Kieu Diem <laptrinh0906@gmail.com>
2026-06-06 13:37:14 +0700

    upgrade: state

--- array&hash/max_sub/kadane.py
+++ array&hash/max_sub/kadane.py
@@ -1,13 +1,31 @@
-nums = [-2,1,-3,4,-1,2,1,-5,4]
+nums = [-5,-2,-8]
 class maxSubArray:
     def kadane(self, nums):
-        current = 0
-        best = float('-inf')
-        for i in range(0, len(nums)):
-            current = max(nums[i], current + nums[i])
-            best = max(current, best)
-        return best
+        current = nums[0]
+        best = nums[0]
+        best_start = 0
+        best_end = 0
+        temp_start = 0
+        #numbers = [0] * len(nums)
+        for i in range(1, len(nums)):
+            if nums[i] > (current + nums[i]):
+                current = nums[i]
+                temp_start = i
+                #numbers[i] = 1
+            else:                
+                current = current + nums[i]
+                #numbers[i] = numbers[i-1] + 1
+            if current > best:
+                best = current
+                best_start = temp_start
+                best_end = i
+            elif current == best:
+                current_len = i - temp_start + 1
+                best_len = best_end - best_start + 1
+                if current_len > best_len:
+                    best_start = temp_start
+                    best_end = i        
+        return best, nums[best_start:best_end+1], current
 
 solver = maxSubArray()
-solver.kadane(nums)
 print(solver.kadane(nums))
